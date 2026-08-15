aed50321c24e7c73b305d93d532d809b2a31dc55
Cody Simonds <36618932+CodySimonds65@users.noreply.github.com>
2026-08-15 17:36:57 -0500

    minor: add automatic client updates (#7)
    * Add automatic client updates
    
    * Trim README guidance

--- client/App.xaml.cs
+++ client/App.xaml.cs
@@ -1,7 +1,31 @@
 using System.Windows;
+using RobloxAltClient.Services;
 
 namespace RobloxAltClient;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        if (UpdateService.IsApplyUpdateMode(e.Args))
+        {
+            try
+            {
+                UpdateService.ApplyUpdate(e.Args);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(
+                    exception.Message,
+                    "Roblox Alt Client update failed",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+
+            Shutdown();
+            return;
+        }
+
+        base.OnStartup(e);
+    }
 }
